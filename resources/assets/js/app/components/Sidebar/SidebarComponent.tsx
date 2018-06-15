4dd89cf987a54b0c40b27b073dcb58c3dcc8dc31
vygandas <vygandas.pliasas@gmail.com>
2018-06-15 20:07:42 +0300

    Support for social icons urls and small cleanup of generated assets

--- resources/assets/js/app/components/Sidebar/SidebarComponent.tsx
+++ resources/assets/js/app/components/Sidebar/SidebarComponent.tsx
@@ -35,26 +35,38 @@ class SidebarComponent extends Component<{}, {}> {
 
                 <section className="vp-sidebar-contacts">
                     <ul>
+                        {appDataHelper('social_link_facebook') &&
                         <li>
-                            <a href="">
-                                <SVG src="/icon/ic_facebook_gray.svg" onLoad={(src) => {}}>Facebook</SVG>
+                            <a href={appDataHelper('social_link_facebook')} target="_blank" rel="nofollow">
+                                <SVG src="/icon/ic_facebook_gray.svg" onLoad={(src) => {
+                                }}>Facebook</SVG>
                             </a>
                         </li>
+                        }
+                        {appDataHelper('social_link_youtube') &&
                         <li>
-                            <a href="">
-                                <SVG src="/icon/if_youtube_circle_gray_107133.svg" onLoad={(src) => {}}>Youtube</SVG>
+                            <a href={appDataHelper('social_link_youtube')} target="_blank" rel="nofollow">
+                                <SVG src="/icon/if_youtube_circle_gray_107133.svg" onLoad={(src) => {
+                                }}>Youtube</SVG>
                             </a>
                         </li>
+                        }
+                        {appDataHelper('social_link_linkedin') &&
                         <li>
-                            <a href="">
-                                <SVG src="/icon/if_linkedin_circle_gray_107149.svg" onLoad={(src) => {}}>LinkedIn</SVG>
+                            <a href={appDataHelper('social_link_linkedin')} target="_blank" rel="nofollow">
+                                <SVG src="/icon/if_linkedin_circle_gray_107149.svg" onLoad={(src) => {
+                                }}>LinkedIn</SVG>
                             </a>
                         </li>
+                        }
+                        {appDataHelper('social_link_instagram') &&
                         <li>
-                            <a href="">
-                                <SVG src="/icon/if_instagram_circle_gray_107138.svg" onLoad={(src) => {}}>Instagram</SVG>
+                            <a href={appDataHelper('social_link_instagram')} target="_blank" rel="nofollow">
+                                <SVG src="/icon/if_instagram_circle_gray_107138.svg" onLoad={(src) => {
+                                }}>Instagram</SVG>
                             </a>
                         </li>
+                        }
                     </ul>
                 </section>
 
